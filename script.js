3b0a7dc558843d6ab856259769975ba1878e7b8d
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-06-26 17:30:28 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // --- 0. DOMAIN LOCK (Anti-Steal) ---
 (function() {
-    const authorized = "https://classroom24k-github-io.vercel.app/";
+    const authorized = "https://classroom24k-github-io.vercel.app";
     const currentLoc = window.location.hostname + window.location.pathname;
 
     // Allows 'localhost' for your development, but breaks on any other domain
